31663d01470546835b273eda8df8158885eef17d
mobina <mobinaa.srmdi@gmail.com>
2024-02-13 10:43:17 -0800

    create snake case

---
+++ snakecase.js
@@ -0,0 +1,12 @@
+
+    let str = 'hello world';
+    for (let i = 0; i < str.length; i++) {
+        
+        if (let i = 0 ; i < str.length ; i += 1) {
+            snakeCase += '_';
+        }
+
+    }
+    
+
+console.log(str); 
