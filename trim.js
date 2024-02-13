85ca158900d1d459f35e835cbd2f8e91d49b45e2
mobina <mobinaa.srmdi@gmail.com>
2024-02-13 10:24:48 -0800

    trim commit

---
+++ trim.js
@@ -0,0 +1,11 @@
+let str = '  hiii i am mobina         '
+result = ''
+for(let i = 0 ; i < str.length ; i += 1) {
+    if(str[i] === '') {
+
+    }else{
+        result += str[i]
+    }
+
+}
+console.log(result)
